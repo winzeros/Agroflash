2814c6f7ee52744839d18cee1a18177ec4cbf92f
brayan <bra_yan_1_x@hotmail.com>
2014-10-28 01:11:28 -0300

    Ultima prueba

--- src/com/principal/agroflash/MainActivity.java
+++ src/com/principal/agroflash/MainActivity.java
@@ -4,13 +4,19 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
+	int x,y;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		x=0;
+		y=1;
+		y=x+y;
+		
 	}
 
 	@Override
